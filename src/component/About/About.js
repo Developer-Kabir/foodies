5862cc829e5f8bb1d2e6b1fffb73be561d0d7dd8
Kabir Hossain <hakabir40@gmail.com>
2022-04-18 20:16:49 +0600

    footer design done

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about page</h2>
+        <div className='about'>
+            <h2 className='fw-bold text-center mb-4'>About Me</h2>
+            <div className='about-grid'>
+                <div>
+                    
+                </div>
+                <div className='about-me'>
+                    <p>Hey, This is Kabir Hossain from Gazipur. I completed my graduation study last month and I am learning front end web development from programming hero . I want to be an excutive web developer after completing this course . I will learn more tecnology for full steack web development . After fiver year I will start a IT company</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Shared Component/Footer/Footer.js
+++ src/component/Shared Component/Footer/Footer.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Home from '../../Home/Home';
-
+import './Footer.css'
 const Footer = () => {
     return (
-        <div>
-
-            <h2>This is footer</h2>
-            <div>
+        <div className='ft'>
+        <div className='footer'>
+            <div className='link-section'>
+                <h2 className='fw-bold'>Useful Link</h2>
                 <div className='useful-link'>
-                    <Link to="/home">Home</Link>
-                    <NavLink path="/home" element={<Home></Home>}></NavLink>
-                    <NavLink path="/home" element={<Home></Home>}></NavLink>
-                    <NavLink path="/home" element={<Home></Home>}></NavLink>
-                    <NavLink path="/home" element={<Home></Home>}></NavLink>
-                    
+                            <div>
+                             <Link className='linkstyle text-dark' to="/home">Home</Link>
+                              <Link className='linkstyle text-dark' to="/about">About</Link>
+                              <Link className='linkstyle text-dark' to="/blog">Blog</Link>
+                            </div>
+                </div>
                 </div>
                 <div className='contact'>
-                    
+                    <h2 className='fw-bold'>Contact Us</h2>
+                    <p>Gazipur-1704 . Gazipr City Corporation</p>
+                    <p>Phone : 0123456789</p>
+                    <p>E-mail : abcdef@geh.com</p>
                 </div>
-            </div>
+        </div>
+        <p className='text-center fw-bold'> All right reserved by Foodies  </p>
         </div>
     );
 };
