209c69dcc2a2a92ee55858f8d3171299effdec0c
Kabir Hossain <hakabir40@gmail.com>
2022-04-19 21:30:58 +0600

    responsiveness done

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <h2 className='fw-bold text-center mb-4'>About Me</h2>
             <div className='about-grid'>
                 <div>
-                    <img width={400} src="https://i.ibb.co/QDVfs3c/IMG20200724170938.jpg" alt=""/>
+                    <img className='img-fluid' src="https://i.ibb.co/QDVfs3c/IMG20200724170938.jpg" alt=""/>
                 </div>
                 <div className='about-me'>
                     <p>Hey, This is Kabir Hossain from Gazipur. I completed my graduation study last month and I am learning front end web development from programming hero . I want to be an excutive web developer after completing this course . I will learn more tecnology for full steack web development . After fiver year I will start a IT company</p>
